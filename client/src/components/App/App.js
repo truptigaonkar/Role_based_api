f3f339c085665c2399c5ed34132c1212779776d1
truptigaonkar <trups11.trupti@gmail.com>
2020-08-09 13:49:40 +0200

    Routing performed using react-router-dom

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
+import Home from '../Home';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
+import Dashboard from '../Auth/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h1>app</h1>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/dashboard" component={Dashboard}/>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h4>Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eum alias quae, quo rerum exercitationem porro esse ex perspiciatis nihil, expedita maxime? Veritatis earum voluptatum nisi nam, nostrum facilis praesentium!</p>
+        </div>
+    )
+}
+
+export default Home;
